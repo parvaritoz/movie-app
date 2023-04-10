377fa8c7ebc24c76f6b985db6407305e312400d0
parvaritoz <rezaparvar9999@gmail.com>
2023-04-10 20:50:07 +0200

    Added footer and navbar to all pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,43 @@
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import type { NextPage } from "next";
+import { ReactElement, ReactNode } from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
+export type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+/** export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="bg-black min-h-screen">
-      <Component {...pageProps} />
+      <Component {...pageProps} />;
+    </div>
+  );
+}*/
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout =
+    Component.getLayout ??
+    ((page) => {
+      return (
+        <>
+          <div className="mx-4 md:mx-6">
+            <Navbar />
+            {page}
+            <Footer />
+          </div>
+        </>
+      );
+    });
+  return (
+    <div className="bg-gray-900 min-h-screen">
+      {getLayout(<Component {...pageProps} />)}
     </div>
   );
 }
