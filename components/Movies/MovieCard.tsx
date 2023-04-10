ec680e5668de97d26a4807e2424fdec119df7aa9
parvaritoz <rezaparvar9999@gmail.com>
2023-04-10 21:19:24 +0200

    Updated movieCard

--- components/Movies/MovieCard.tsx
+++ components/Movies/MovieCard.tsx
@@ -1,39 +1,51 @@
-import React from "react";
+import { Movie } from "@/utils/Type";
+import { useRouter } from "next/router";
 
 const getImgULR = (path: string) => {
   return `https://www.themoviedb.org/t/p/w440_and_h660_face${path}`;
 };
 
-interface movieCardProps {
-  poster_path: string;
-  original_title: string;
-  release_date: string;
-}
+type MovieDetailsProps = {
+  movie: Movie;
+};
 
-const MovieCard = ({
-  poster_path,
-  original_title,
-  release_date,
-}: movieCardProps) => {
+const MovieCard = ({ movie }: MovieDetailsProps) => {
+  const router = useRouter();
+
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    console.log("Movie ID: ", movie.id);
+    console.log("Movie Title: ", movie.original_title);
+    console.log("Overview: ", movie.overview);
+    console.log("Date: ", movie.release_date);
+    // Add your code to handle the click event here
+  };
+
   return (
     <>
       <div className="flex flex-col md:flex-row h-full">
-        <div className="w-[200px] h-[325px] md:w-[300px] md:h-[450px] relative mr-4">
-          <div className="absolute z-0 hover:opacity-20">
-            <img
-              src={getImgULR(poster_path)}
-              alt="imgPath"
-              className="rounded-lg"
-            />
+        <div
+          className="w-[200px] h-[325px] md:w-[250px] md:h-[375px] relative mr-4"
+          onClick={handleClick}
+        >
+          <div className="absolute z-0 hover:opacity-30">
+            {movie.poster_path && (
+              <img
+                src={getImgULR(movie.poster_path)}
+                alt="imgPath"
+                className="rounded-lg"
+              />
+            )}
           </div>
           <div className="flex flex-col justify-center h-full">
             <div className="flex justify-center items-center">
               <h1 className="text-white text-md md:text-lg">
-                {original_title}
+                {movie.original_title}
               </h1>
             </div>
             <div className="flex justify-center">
-              <p className="items-end text-white text-md">{release_date}</p>
+              <p className="items-end text-white text-md">
+                {movie.release_date}
+              </p>
             </div>
           </div>
         </div>
