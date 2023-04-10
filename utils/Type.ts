1ec7c614b8b021dbfa75898c75a08ec6be0a3bee
parvaritoz <rezaparvar9999@gmail.com>
2023-04-10 20:49:21 +0200

    Added config values and created a movie type

---
+++ utils/Type.ts
@@ -0,0 +1,12 @@
+export type Movie = {
+  id: number;
+  original_title: string;
+  poster_path: string;
+  release_date: string;
+  overview: string;
+};
+
+export type Genre = {
+  id: string;
+  name: string;
+};
