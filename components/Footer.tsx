6e7ccf0ca9ffbb9e6e7794fe9f628b3b332bc286
parvaritoz <rezaparvar9999@gmail.com>
2023-04-10 20:50:15 +0200

    Added footer

---
+++ components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="flex justify-center text-white mt-8">
+      <p className="mb-4 italic text-xs">
+        This product uses the TMDb API but is not endorsed or certified by TMDb.
+        Created by Reza Parvar.
+      </p>
+    </footer>
+  );
+}
