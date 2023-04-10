b1af97d51ae23cc186c473eb6bbadf3c3ebc4738
parvaritoz <rezaparvar9999@gmail.com>
2023-04-10 21:21:51 +0200

    Added a search component

--- components/Search.tsx
+++ components/Search.tsx
@@ -1,19 +1,40 @@
-import Link from "next/link";
-import React from "react";
+import { useState } from "react";
+import { useRouter } from "next/router";
 import { AiOutlineSearch } from "react-icons/ai";
+import Link from "next/link";
 
 export default function Search() {
+  const [query, setQuery] = useState("");
+  const router = useRouter();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (query.trim() === "") {
+      return;
+    }
+
+    await router.push(`/search?query=${encodeURIComponent(query)}`);
+  };
+
   return (
-    <div className="rounded-2xl bg-white mt-4">
-      <div className="flex flex-row items-center">
-        <Link href={""}>
-          <AiOutlineSearch size={30} className="ml-2" />
+    <div className="rounded-2xl bg-white mt-4 md:mt-0">
+      <form className="flex flex-row items-center" onSubmit={handleSubmit}>
+        <Link href="/search">
+          <AiOutlineSearch size={27} className="ml-2" type={"submit"} />
         </Link>
         <input
-          className="w-full rounded-2xl p-2 outline-none"
-          placeholder="Search movie titles..."
+          type="text"
+          value={query}
+          onChange={handleChange}
+          className="rounded-2xl p-1 outline-none w-[82%]"
+          placeholder="Search for a movie..."
         />
-      </div>
+      </form>
     </div>
   );
 }
