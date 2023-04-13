17464c191103a9c1f9063206f8e6864f551af577
parvaritoz <rezaparvar9999@gmail.com>
2023-04-13 19:43:44 +0200

    Added keys from .env.local

--- utils/api.ts
+++ utils/api.ts
@@ -1,14 +1,16 @@
 import axios from "axios";
 import { Movie } from "../utils/Type";
-import { API_KEY, API_URL } from "@/config/config";
 
 export async function searchMovies(query: string): Promise<Movie[]> {
-  const { data } = await axios.get(`${API_URL}/search/movie`, {
-    params: {
-      api_key: API_KEY,
-      query,
-    },
-  });
+  const { data } = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/search/movie`,
+    {
+      params: {
+        api_key: process.env.NEXT_PUBLIC_TMDB_API_KEY,
+        query,
+      },
+    }
+  );
 
   return data.results.map((result: any) => ({
     id: result.id,
