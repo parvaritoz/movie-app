f98b625368278b1f259df6f075365ae9b3cd384a
parvaritoz <rezaparvar9999@gmail.com>
2023-04-13 19:51:14 +0200

    Removed unnecessary nodes and added `&apos;` for deployment

--- pages/search.tsx
+++ pages/search.tsx
@@ -5,9 +5,7 @@ import { useEffect, useState } from "react";
 import { searchMovies } from "../utils/api";
 import { Movie } from "../utils/Type";
 
-interface Props {}
-
-export default function SearchResultsPage(props: Props) {
+export default function SearchResultsPage() {
   const router = useRouter();
   const { query } = router.query;
   const [results, setResults] = useState<Movie[]>([]);
@@ -25,7 +23,9 @@ export default function SearchResultsPage(props: Props) {
   if (results.length === 0) {
     return (
       <div className="flex justify-center mt-10">
-        <p className="text-white text-2xl">No results found for '{query}'</p>
+        <p className="text-white text-2xl">
+          No results found for `&apos;`{query}`&apos;`
+        </p>
       </div>
     );
   }
@@ -41,7 +41,7 @@ export default function SearchResultsPage(props: Props) {
       <div>
         <div>
           <h1 className="text-white mt-4 mb-4 text-3xl">
-            Search Results for '{query}'
+            Search Results for `&apos;`{query}`&apos;`
           </h1>
           <div className="flex flex-row overflow-scroll text-white">
             {results.map((movie, key) => (
@@ -49,22 +49,6 @@ export default function SearchResultsPage(props: Props) {
             ))}
           </div>
         </div>
-        {/**{results.map((movie) => (
-        <div key={movie.id}>
-         <Link href={`/movie/${movie.id}`}>
-            {movie.poster_path && (
-              <img
-                src={`https://image.tmdb.org/t/p/w185/${movie.poster_path}`}
-                alt={movie.original_title}
-                width={185}
-                height={278}
-              />
-            )}
-            <h2>{movie.original_title}</h2>
-            <p>{movie.release_date}</p>
-          </Link>
-        </div>
-      ))}*/}
       </div>
     </>
   );
